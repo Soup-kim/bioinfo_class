d5694145197c493650f7d966cbde2b81a6d0c979
SoupKim <kimwoods_s@snu.ac.kr>
2024-05-09 17:25:21 +0900

    draw integral membrane graph

--- 2.py
+++ 2.py
@@ -14,3 +14,12 @@ print(unique_values)
 merged_int = merged[merged['type'] == 'integral membrane']
 merged_nu = merged[merged['type'] == 'nucleus']
 merged_cyt = merged[merged['type'] == 'cytoplasm']
+
+# integral membrane 먼저 그려보기
+from matplotlib import pyplot as plt
+import numpy as np
+
+x_i = np.log2(merged_int['clip_enrichment'])
+y_i = np.log2(merged_int['rden_change'])
+
+plt.scatter(x_i, y_i, s=1, alpha =.4, color = 'r', label = 'integral membrane')
