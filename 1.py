158e327b1daeb6046e16b6ecc0c954d54f56e7c9
SoupKim <kimwoods_s@snu.ac.kr>
2024-05-09 16:05:31 +0900

    modify r-value print

--- 1.py
+++ 1.py
@@ -12,4 +12,4 @@ ax.set_ylabel('Ribosome density change (log₂)')
 
 mask = np.isfinite(cnts['clip_enrichment']) & np.isfinite(cnts['rden_change']) & (cnts['clip_enrichment'] > 0) & (cnts['rden_change'] > 0)
 r, _ = pearsonr(np.log2(cnts['clip_enrichment'][mask]), np.log2(cnts['rden_change'][mask]))
-plt.text(0.1, 0.9, f'r = {r:.2f}', transform=plt.gca().transAxes)
+plt.text(0.1, 0.9, f'r = {r:.4f}', transform=plt.gca().transAxes)
