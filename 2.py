70781b401f4c5a846663819ec1aaa8ba3b0a13eb
SoupKim <kimwoods_s@snu.ac.kr>
2024-05-09 17:09:22 +0900

    check type list

--- 2.py
+++ 2.py
@@ -4,3 +4,7 @@ cnts['index_int'] = cnts.index.astype(str).str.extract(r'(\d+)', expand=False).a
 mouselocal['index_int'] = mouselocal.index.astype(str).str.extract(r'(\d+)', expand=False).astype(int)
 merged = pd.merge(cnts, mouselocal, on='index_int', how='inner')
 merged.head()
+
+# type 종류 확인
+unique_values = merged['type'].unique()
+print(unique_values)
