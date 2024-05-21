f703e60fad4daaa54c9a106c6356bab5b5c5ae21
SoupKim <kimwoods_s@snu.ac.kr>
2024-05-21 23:09:14 +0900

    delete other than acgt

--- Guided_Mission_3/mission3.py
+++ Guided_Mission_3/mission3.py
@@ -2,3 +2,8 @@
 base_count = pileup.groupby(['pos'])['matches'].apply(lambda x: pd.Series(list(''.join(x))).value_counts()).unstack().fillna(0)
 base_count.head(10)
 
+#ACGT 이외의 알파벳이 나와서 제거
+def filter_bases(matches):
+    return ''.join(base for base in matches if base in 'ACGT')
+
+pileup['matches'] = pileup['matches'].apply(filter_bases)
