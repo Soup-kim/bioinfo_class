20907df65dc6fef70ce772b19eb8f97a6907f45e
SoupKim <kimwoods_s@snu.ac.kr>
2024-05-21 23:22:12 +0900

    Shannon entropy

--- Guided_Mission_3/mission3.py
+++ Guided_Mission_3/mission3.py
@@ -1,9 +1,21 @@
 #base 수 세기
-base_count = pileup.groupby(['pos'])['matches'].apply(lambda x: pd.Series(list(''.join(x))).value_counts()).unstack().fillna(0)
-base_count.head(10)
+base_counts = pileup.groupby(['pos'])['matches'].apply(lambda x: pd.Series(list(''.join(x))).value_counts()).unstack().fillna(0)
+base_counts.head(10)
 
 #ACGT 이외의 알파벳이 나와서 제거
 def filter_bases(matches):
     return ''.join(base for base in matches if base in 'ACGT')
 
 pileup['matches'] = pileup['matches'].apply(filter_bases)
+
+# Shannon entropy
+import numpy as np
+
+def shannon_entropy(base_counts):
+    total_counts = base_counts.sum()
+    probs = base_counts / total_counts
+    return -np.sum(probs * np.log2(probs))
+
+# 각 position별 Shannon entropy
+entropies = base_counts.apply(shannon_entropy, axis=1)
+entropies.head(30)
