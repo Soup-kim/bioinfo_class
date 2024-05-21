46a97cff877a47b1f7ec30dcd26ac9aa0869b5fc
SoupKim <kimwoods_s@snu.ac.kr>
2024-05-21 23:38:37 +0900

    modify base counts code (empty matches)

--- Guided_Mission_3/mission3.py
+++ Guided_Mission_3/mission3.py
@@ -1,5 +1,17 @@
 #base 수 세기
-base_counts = pileup.groupby(['pos'])['matches'].apply(lambda x: pd.Series(list(''.join(x))).value_counts()).unstack().fillna(0)
+def count_bases(matches):
+    base_counts = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
+    
+    # matches가 비어있는 경우 처리
+    if matches.empty:
+        return pd.Series(base_counts)
+    
+    for match in matches:
+        for base in match:
+            base_counts[base] += 1
+    return pd.Series(base_counts)
+
+base_counts = pileup.groupby('pos')['matches'].apply(count_bases).unstack().fillna(0)
 base_counts.head(10)
 
 #ACGT 이외의 알파벳이 나와서 제거
@@ -7,6 +19,7 @@ def filter_bases(matches):
     return ''.join(base for base in matches if base in 'ACGT')
 
 pileup['matches'] = pileup['matches'].apply(filter_bases)
+
 
 # Shannon entropy
 import numpy as np
